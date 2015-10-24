f6a1bd66f7b639b446693d334794f9ddfe46e567
kangsoontan <tankangsoon@gmail.com>
2015-10-24 16:33:07 -0700

    checkout.js and list.js now can modify quantity, and show only when quantity > 0

--- client/list.js
+++ client/list.js
@@ -1,9 +1,24 @@
 Template.list.helpers({
 	Items: function(){
-		return Items.find();
+		return Items.find({quantity: { $gt: 0}});
 	}
 });
 
 Template.list.events({
-	
+	"click #addToCart": function(event) {
+		Carts.insert({buyerId: Meteor.user()._id, itemId: this._id});
+
+		var title = Items.findOne({_id: this._id}).title;
+		var price = Items.findOne({_id: this._id}).price;
+		var image = Items.findOne({_id: this._id}).image;
+		var description = Items.findOne({_id: this._id}).description;
+		var quantity = parseInt(Items.findOne({_id: this._id}).quantity)-1;
+		Items.update({_id: this._id},{
+			title: title,
+			price: price,
+			image, image,
+			description: description,
+			quantity: quantity
+		});
+	}
 });
