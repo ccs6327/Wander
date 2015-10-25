d2c9e954f34a1a418582aad7e0bcfd04801f61c1
kangsoontan <tankangsoon@gmail.com>
2015-10-25 06:24:58 -0700

    Styled the indexed store page

--- client/shop.js
+++ client/shop.js
@@ -2,5 +2,29 @@ Template.shop.helpers({
 	Items: function(){
 		var shopId = this._id;
 		return Items.find({quantity: {$gt: 0}}, {shopId: shopId}); 
+	},
+	likes: function(){
+		var shopId = this._id;
+		return Shops.findOne({_id: this._id})["likes"];
+	},
+	dislikes: function(){
+		var shopId = this._id;
+		return Shops.findOne({_id: this._id})["dislikes"];
+	},
+	sellerEmail: function(){
+		var shopId = this._id;
+		owner = Shops.findOne({_id: this._id})["owner"];
+		return Meteor.users.findOne({_id: owner}).emails[0].address;
+	}
+});
+
+Template.shop.events({
+	"click #addToCart": function(event) {
+		Carts.insert({buyerId: Meteor.user()._id, itemId: this._id});
+
+		var quantity = parseInt(Items.findOne({_id: this._id}).quantity)-1;
+		Items.update({_id: this._id},{ $set: {quantity: quantity}});
+
+		Router.go('/checkout');
 	}
 });
